51b5bf5a4251c9aca1ed28178368aaab97ae1aed
Fabian Marriaga <fabiancamilo15@msn.com>
2024-04-22 20:41:34 -0500

    Vista, controlador, ruta, Clientes

--- database/migrations/2024_04_22_224816_create_clientes_table.php
+++ database/migrations/2024_04_22_224816_create_clientes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('clientes', function (Blueprint $table) {
-            $$table->id();
+            $table->id();
             $table->string('nombre');
             $table->string('apellido');
             $table->string('telefono');
